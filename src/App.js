bd16c86c88dfe95c0e6e65256cf293c5396a1f57
keisukeadachi0819 <92622310+keisukeadachi0819@users.noreply.github.com>
2022-06-29 00:37:12 +0900

    useState、useEffect、useContext、useRefの確認

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useContext, useEffect, useRef, useState } from 'react';
+import UserInfoContext from '.';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const userInfo = useContext(UserInfoContext);
+  const ref = useRef();
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    console.log("Hello World!");
+    // 無限ループ
+    // setCount(count + 1);
+  }, [count])
+
+  const handleRef = () => {
+    console.log(ref);
+    console.log("Current Input Value : ",ref.current.value);
+    console.log("Current Input Element Offset-Height : ",ref.current.offsetHeight);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hooks</h1>
+      <div>
+        <h2>UseState, UseEffect</h2>
+        <button onClick={handleClick}>+</button>
+        <p>{count}</p>
+      </div>
+      <hr />
+      <div>
+        <h2>UseContext</h2>
+        <div>
+          <h3>UserInfo</h3>
+          <p>{userInfo.name}</p>
+          <p>{userInfo.age}</p>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h2>UseRef</h2>
+        <input type="text" ref={ref} />
+        <button onClick={handleRef}>Check the value on console!</button>
+      </div>
     </div>
   );
 }
